0ac09646a6dc0541164afa0287e10853bdae342e
sangyouh <sangyouh@usc.edu>
2023-07-10 03:23:32 +0000

    Deploying to gh-pages from @ sangyouh/ssg@a95a7a28161ad1b0dfb62eb2d732a76804036b82 🚀

--- github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/next.config.js
+++ github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/github-pages-deploy-action-temp-deployment-folder/next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {}
+
 module.exports = {
     env: {
         NOTION_KEY: process.env.NOTION_KEY,
         NOTION_DATABASE: process.env.NOTION_DATABASE,
-    },
-    output: "export"
+    }
 }
